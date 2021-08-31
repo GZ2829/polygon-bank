180d580326f844af8c23651c472e57a240c76b75
Greg Zucchero <gzucchero24@gmail.com>
2021-08-30 21:46:58 -0400

    add console log

--- blockchain/test/test-bank.js
+++ blockchain/test/test-bank.js
@@ -5,6 +5,8 @@ describe('Bank App', () => {
     let bank, token, owner, address_1, address_2;
     let addresses;
 
+    console.log('heeloo there')
+
     beforeEach(async ()=>{
         const BankContract = await ethers.getContractFactory("Bank")
     })
