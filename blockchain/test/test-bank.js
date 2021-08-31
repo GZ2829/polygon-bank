37c1fe47a00b9c210ee466d866f1086596b41ccb
Greg Zucchero <gzucchero24@gmail.com>
2021-08-31 19:57:11 -0400

    finsihed chai expect

--- blockchain/test/test-bank.js
+++ blockchain/test/test-bank.js
@@ -19,12 +19,21 @@ describe('Bank App', () => {
 
         describe("Deployment", () =>{
             it('Should have zero assets', async ()=>{
-                expect(await bank.totalAssets().to.equal("0"))
+                expect(await bank.totalAssets()).to.equal("0")
             });
             it("Should have 0 tokens and 0 deposit in owner account", async ()=>{
                 expect(await bank.accounts(owner.address)).to.equal("0")
                 expect(await token.balanceOf(owner.address)).to.equal("0")
             });
+            it('Should have zero tokens and zero deposit for address_1', async ()=>{
+                expect(await bank.accounts(address_1.address)).to.equal("0")
+                expect(await token.balanceOf(address_1.address)).to.equal("0")
+            });
+            it('Should have zero tokens and zero deposit for address_2', async ()=>{
+                expect(await bank.accounts(address_2.address)).to.equal("0")
+                expect(await token.balanceOf(address_2.address)).to.equal("0")
+            });
+
         })
 
 
