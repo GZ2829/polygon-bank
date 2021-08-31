cf3a11f5885e8292877224fe04fea33270065de5
Greg Zucchero <gzucchero24@gmail.com>
2021-08-31 19:38:40 -0400

    beginning of chai

--- blockchain/test/test-bank.js
+++ blockchain/test/test-bank.js
@@ -5,10 +5,29 @@ describe('Bank App', () => {
     let bank, token, owner, address_1, address_2;
     let addresses;
 
-    console.log('heeloo there')
-
     beforeEach(async ()=>{
-        const BankContract = await ethers.getContractFactory("Bank")
+        const BankContract = await ethers.getContractFactory("Bank");
+        bank = await BankContract deploy(),
+        await bank deployed()
+
+        const TokenContract = await ethers.getContractFactory("Token");
+        token = await BankContract deploy(bank.address),
+        await token deployed();
+
+        [owner, address_1, address_2, ...addresses] = await ethers.getSigners()
+
+
+        describe("Deployment", () =>{
+            it('Should have zero assets', async ()=>{
+                expect(await bank.totalAssets().to.equal("0"))
+            });
+            it("Should have 0 tokens and 0 deposit in owner account", async ()=>{
+                expect(await bank.accounts(owner.address)).to.equal("0")
+                expect(await token.balanceOf(owner.address)).to.equal("0")
+            });
+        })
+
+
     })
 
 
